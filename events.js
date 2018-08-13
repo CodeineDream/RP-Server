d5ed068e60e72d2fd2c2593a66caeaffbb2e8834
Jax Ferguson <42292185+CodeineDream@users.noreply.github.com>
2018-08-13 10:53:01 +0100

    Update events.js
    Got spawn at start working.

--- events.js
+++ events.js
@@ -1,7 +1,7 @@
 let spawnPoints = require('./spawn_points.json').SpawnPoints;
 
 mp.events.add("playerJoin", (player) => {
-	player.position(x, y, z) == (-104.500, 6315.787, 31.576);
+	player.spawn(new mp.Vector3(-104.500,6315.787,31.576));
 });
 
 mp.events.add('playerDeath', (player) => {
